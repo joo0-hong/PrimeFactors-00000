a107f79d15a6a7913e429e33cb84d5015a35a766
jooyoung <hjoo820@naver.com>
2024-07-03 11:07:10 +0900

    Cycle[3]-REFACTOR, Page 42

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 class PrimeFactor {
 public : 
-	vector<int> of(int i) {
+	vector<int> of(int num) {
 		vector<int> result = {};
-		if (i == 2) result.push_back(2);
-		if (i == 3) result.push_back(3);
+		if (num > 1) result.push_back(num);
 		return result;
 	}
 };
