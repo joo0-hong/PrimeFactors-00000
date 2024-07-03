bae5451207317142dfe6f392960d7c8b551b70c7
jooyoung <hjoo820@naver.com>
2024-07-03 11:15:53 +0900

    Cycle[5]-REFACTOR, Page 50

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -7,16 +7,17 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {};
 		if (num > 1) {
+			int divisor = 2;
 			if (num == 4) {
-				while (num % 2 == 0) {
+				while (num % divisor == 0) {
 					result.push_back(2);
-					num /= 2;
+					num /= divisor;
 				}
 			}
 			else if (num == 6) {
-				while (num % 2 == 0) {
+				while (num % divisor == 0) {
 					result.push_back(2);
-					num /= 2;
+					num /= divisor;
 				}
 				while (num % 3 == 0) {
 					result.push_back(3);
